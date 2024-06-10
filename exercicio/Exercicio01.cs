bec7fd5f25d685c3b365333154e145c4d99dbe0e
milena <milena-.dias@live.com>
2024-06-10 12:03:37 -0300

    Access Strings

---
+++ exercicio/Exercicio01.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Exercicio01
+{
+    public static void Main(string[] args)
+    {
+        string firstName = "John";
+        string lastName = "Doe";
+        string name = $"My full name is: {firstName} {lastName}";
+        Console.WriteLine(name);
+    }
+}
---
+++ exercicio01/Exercicio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Exercicio
+{
+    public static void Main(string[] args)
+    {
+        string name = "John Doe";
+
+        int charPos = name.IndexOf("D");
+        
+        string lastName = name.Substring(charPos);
+
+        Console.WriteLine(lastName);
+    }
+}
